ea3c0d4ae496e6a9c870c73097602901630c96f4
Dawid Spisak <dawidspisak@gmail.com>
2023-11-14 00:32:54 +0100

    feat: include fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { NavigationList } from "@/components/navigation/NavigationList";
+"use client";
 import { Navigation } from "@/components/navigation/Navigation";
+import { NavigationProvider } from "@/context/NavigationProvider";
 
 export default function Home() {
   return (
-    <main className="bg-red-100">
-      <Navigation />
+    <main className="bg-red-100 h-[300vh]">
+      <NavigationProvider>
+        <Navigation />
+      </NavigationProvider>
     </main>
   );
 }
