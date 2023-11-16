a36cb62855fcf478df38dbc8fcdca0ae88c879f6
Dawid Spisak <dawidspisak@gmail.com>
2023-11-16 19:49:58 +0100

    feat: company

---
+++ src/components/organisms/footer/company/Company.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Heading } from 'components/atoms/heading/Heading';
+import { Text } from 'components/atoms/text/Text';
+
+export const Company = () => (
+  <div className="flex flex-col justify-center gap-4 border-l border-[#494B41] px-4">
+    <Heading size="h4">Dane firmy</Heading>
+    <div className="flex flex-col">
+      <Text>NIP: 5492470598</Text>
+      <Text>Kamil Hajduk Strony Internetowe</Text>
+      <Text>ul. Wojska Polskiego 1b./10</Text>
+      <Text>32-640 Zator</Text>
+    </div>
+  </div>
+);
