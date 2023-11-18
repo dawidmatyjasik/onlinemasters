7eeac4ab698e3c5b9e0c2c0dde6665bbe24716ce
Dawid Spisak <dawidspisak@gmail.com>
2023-11-18 19:25:59 +0100

    feat: line

---
+++ src/components/atoms/line/Line.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Line = () => <div className="h-[1px] w-full bg-[rgb(73,75,65)]" />;
--- src/components/organisms/footer/copyright/Copyright.tsx
+++ src/components/organisms/footer/copyright/Copyright.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Container } from 'components/atoms/container/Container';
+import { Line } from 'components/atoms/line/Line';
 import { Link } from 'components/atoms/link/Link';
 import { Text } from 'components/atoms/text/Text';
 
 export const Copyright = () => (
   <Container className="flex flex-col items-center gap-6">
-    <div className="h-[1px] w-full bg-[rgb(73,75,65)]" />
+    <Line />
     <Text color="footer" size="small">
       © 2023 Designed by <Link href="https://onlinemasters.pl">Online Masters</Link> |{' '}
       <Link href="https://onlinemasters.pl/polityka-prywatnosci/">Polityka prywatności</Link>
