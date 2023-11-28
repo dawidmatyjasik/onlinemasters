eddb18996ceb88b7983a943c92d9e0ae9838aa73
Dawid Spisak <dawidspisak@gmail.com>
2023-11-28 23:10:57 +0100

    feat: stepbox

---
+++ src/components/molecules/hero/components/step/Step.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import { Text } from 'components/atoms/text/Text';
+
+import type { StepType } from '../stepSection/StepSection';
+
+export const Step = ({ icon, description, alt }: Omit<StepType, 'id'>) => (
+  <div className="flex items-center gap-3">
+    <Image src={icon} className="w-8" alt={alt} />
+    <Text as="h6">{description}</Text>
+  </div>
+);
