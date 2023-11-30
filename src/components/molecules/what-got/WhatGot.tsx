92dd11987671b4162460698ac5948ddd135325c1
Dawid Spisak <dawidspisak@gmail.com>
2023-11-30 23:06:48 +0100

    feat: what got

---
+++ src/components/molecules/what-got/WhatGot.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { WhatGotCards } from './components/WhatGotCard';
+
+import { useTranslation } from 'app/i18n';
+import { Card } from 'components/atoms/card/Card';
+import { Container } from 'components/atoms/container/Container';
+import { Text } from 'components/atoms/text/Text';
+
+const WHAT_GOT_STRUCTURE = [
+  { src: '/svg/support-svgrepo-com.svg', title: '1.title', description: '1.description' },
+  { src: '/svg/website-theme-svgrepo-com.svg', title: '2.title', description: '2.description' },
+  { src: '/svg/content-right-svgrepo-com.svg', title: '3.title', description: '3.description' },
+  { src: '/svg/iphone-svgrepo-com.svg', title: '4.title', description: '4.description' },
+  { src: '/svg/handshake-deal-svgrepo-com.svg', title: '5.title', description: '5.description' },
+];
+
+export const WhatGot = async () => {
+  const { t } = await useTranslation('whatGot');
+
+  return (
+    <Container>
+      <section className="grid grid-cols-3 grid-rows-2 gap-8">
+        <Text as="h2">{t('title')}</Text>
+        {WHAT_GOT_STRUCTURE.map(({ src, title, description }) => (
+          <Card key={title}>
+            <WhatGotCards src={src} title={title} description={description} />
+          </Card>
+        ))}
+      </section>
+    </Container>
+  );
+};
---
+++ src/components/molecules/what-got/components/WhatGotCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+import { useTranslation } from 'app/i18n';
+import { Text } from 'components/atoms/text/Text';
+
+interface WhatGotCardsProps {
+  src: string;
+  title: string;
+  description: string;
+}
+
+export const WhatGotCards = async ({ description, src, title }: WhatGotCardsProps) => {
+  const { t } = await useTranslation('whatGot.cards');
+  return (
+    <div className="flex flex-col gap-2">
+      <Image src={src} alt="TODO" width={38} height={38} />
+      <Text weight="medium" as="h3">
+        {t(title)}
+      </Text>
+      <Text>{t(description)}</Text>
+    </div>
+  );
+};
