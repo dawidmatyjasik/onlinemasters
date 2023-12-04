a1e4ceecda653ecd046fc082a7e833d05747523d
Dawid Spisak <dawidspisak@gmail.com>
2023-12-04 17:46:55 +0100

    feat: handle icons

--- src/components/molecules/taking-look/components/TakingLookContent.tsx
+++ src/components/molecules/taking-look/components/TakingLookContent.tsx
@@ -4,6 +4,7 @@ import { ChevronRight } from 'lucide-react';
 
 import { useTranslation } from 'app/i18n';
 import { Divider } from 'components/atoms/divider/Divider';
+import { Step } from 'components/atoms/step/Step';
 import { Text } from 'components/atoms/text/Text';
 import { Button } from 'components/ui/button';
 
@@ -13,8 +14,10 @@ export const TakingLookContent = async () => {
     <article className="flex flex-col justify-center gap-6">
       <Text as="h2">{t('takingLook.title')}</Text>
       <Divider />
+      <Step img="/svg/star-svgrepo-com.svg" hideArrow hideStep className="h-24 w-24" />
       <Text as="h3">{t('takingLook.subTitle1')}</Text>
       <Text>{t('takingLook.subDescription1')}</Text>
+      <Step img="/svg/support-svgrepo-com.svg" hideArrow hideStep className="h-24 w-24" />
       <Text as="h3">{t('takingLook.subTitle2')}</Text>
       <Text>{t('takingLook.subDescription2')}</Text>
       <Button size="md" variant="primary" className="w-[260px]" iconStart={<ChevronRight />}>
