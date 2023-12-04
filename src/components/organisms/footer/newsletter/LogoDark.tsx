eaa9f005fa0294f4b3c18cf915fc489ec7053703
Dawid Spisak <dawidspisak@gmail.com>
2023-12-04 17:41:57 +0100

    fix: legacy attributes

--- src/components/organisms/footer/newsletter/LogoDark.tsx
+++ src/components/organisms/footer/newsletter/LogoDark.tsx
@@ -5,8 +5,12 @@ import Link from 'next/link';
 
 export const LogoDark = () => (
   <Link href="https://onlinemasters.pl/">
-    <div className="relative h-[60px] w-[130px] ">
-      <Image src="/images/webiste/logo-dark.webp" layout="fill" alt="Online Masters logo" className="object-contain" />
-    </div>
+    <Image
+      src="/images/webiste/logo-dark.webp"
+      height={60}
+      width={130}
+      alt="Online Masters logo"
+      className="object-contain"
+    />
   </Link>
 );
