2a1aacae3629937b3124e7698238cab266fdd79d
Dawid Spisak <dawidspisak@gmail.com>
2023-12-06 23:11:15 +0100

    feat: replace images hero

--- src/components/molecules/hero/components/trusted/Trusted.tsx
+++ src/components/molecules/hero/components/trusted/Trusted.tsx
@@ -7,16 +7,34 @@ import { Text } from 'components/atoms/text/Text';
 export const Trusted = () => (
   <article className="flex w-full flex-col justify-center gap-4">
     <div className="flex flex-1 items-center justify-between gap-4">
-      <Image src="/images/hero/pospawane.webp" alt="pospawane" className="object-contain" width={100} height={100} />
-      <Image src="/images/hero/contaria.png" alt="contaria" className="object-contain" width={100} height={100} />
       <Image
-        src="/images/hero/fizjoterapia.png"
-        alt="fizjoterapia"
+        src="/images/hero/pospawane.webp"
+        alt="Strony Internetowe - Online Masters"
         className="object-contain"
         width={100}
         height={100}
       />
-      <Image src="/images/hero/demman.webp" alt="demman" className="object-contain" width={100} height={100} />
+      <Image
+        src="/images/hero/contaria.png"
+        alt="Sklepy Internetowe - Online Masters"
+        className="object-contain"
+        width={100}
+        height={100}
+      />
+      <Image
+        src="/images/hero/fizjoterapia.png"
+        alt="Opieka nad stroną - Online Masters"
+        className="object-contain"
+        width={100}
+        height={100}
+      />
+      <Image
+        src="/images/hero/demman.webp"
+        alt="Social Media - Online Masters"
+        className="object-contain"
+        width={100}
+        height={100}
+      />
     </div>
     <Text as="span" size="small" className="text-center !text-[#8f8f8f]">
       ZAUFALI NAM
