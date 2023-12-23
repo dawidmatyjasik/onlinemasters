a02d225329e121933e93f3aa4823ffa9632beba4
Dawid Spisak <dawidspisak@gmail.com>
2023-12-23 15:16:24 +0100

    fix: sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,9 +4,14 @@ import type { MetadataRoute } from 'next';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const stronyInternetowe = getCityData('strony-internetowe');
+  const opiekaNadStronami = getCityData('opieka-nad-stronami-wordpress');
   const stronyEntries: MetadataRoute.Sitemap = stronyInternetowe.map(({ city }) => ({
     url: `https://oferty.onlinemasters.pl/strony-internetowe/${city}`,
   }));
 
-  return [...stronyEntries];
+  const opiekaEntries: MetadataRoute.Sitemap = opiekaNadStronami.map(({ city }) => ({
+    url: `https://oferty.onlinemasters.pl/opieka-nad-stronami-wordpress/${city}`,
+  }));
+
+  return [...stronyEntries, ...opiekaEntries];
 }
