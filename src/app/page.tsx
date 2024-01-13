f9048d00781539765f251c7b5d02afff288adf42
Dawid Spisak <dawidspisak@gmail.com>
2024-01-13 22:20:42 +0100

    feat: home page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Hero } from 'components/home/hero/Hero';
+import Offer from 'components/home/offer/Offer';
+
+const Home = () => (
+  <>
+    <Hero />
+    <Offer />
+  </>
+);
+
+export default Home;
