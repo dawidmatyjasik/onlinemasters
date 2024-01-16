5c139323da1b21e50868e225b3cfbf606c202e89
Dawid Spisak <dawidspisak@gmail.com>
2024-01-16 12:50:16 +0100

    fix: mobile

--- src/components/pozycjonowanie-wizytowki-google/what-we-like/components/WhatWeLikeMap.tsx
+++ src/components/pozycjonowanie-wizytowki-google/what-we-like/components/WhatWeLikeMap.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Image from 'next/image';
 
 export const WhatWeLikeMap = () => (
-  <article className="mx-auto flex items-center gap-16">
+  <article className="mx-auto flex flex-col items-center gap-4 md:flex-row md:gap-12">
     <div className="relative h-full w-full">
       <Image
         src="/pozycjonowanie-wizytowki/what-we-like/mapka-przed.webp"
         alt="Rezulalt przed pozycjonowamiem wizytówki"
         width={400}
         height={400}
+        className="rounded-[20px]"
       />
     </div>
     <Image src="/svg/chevron-right.svg" alt="Rezulalt po działaniu" width={90} height={90} />
@@ -20,6 +21,7 @@ export const WhatWeLikeMap = () => (
         alt="Rezulalt po pozycjonowaniu wizytówki"
         width={400}
         height={400}
+        className="rounded-[20px]"
       />
     </div>
   </article>
